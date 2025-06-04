532f410679f10c4bb6674cb14aa30a5190ffa92f
Cremacious <chrismackall3@gmail.com>
2025-06-04 18:40:45 -0400

    Setup with tailwind, shadcn, and clerk

---
+++ frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import { Button } from './components/ui/button';
+
+function App() {
+  return (
+    <div className="bg-gray-800 text-white p-4">
+      App
+      <div>
+        <Button>Button</Button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
