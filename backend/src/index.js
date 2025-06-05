ee68931098c9be8048101b548f9852a9a90d51fe
cremacious <chrismackall3@gmail.com>
2025-06-05 10:19:57 -0400

    Stop tracking node_modules

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,9 +3,10 @@ import dotenv from 'dotenv';
 import userRoutes from './routes/user.route.js';
 import authRoutes from './routes/auth.routes.js';
 import adminRoutes from './routes/admin.routes.js';
-import songsRoutes from './routes/songs.routes.js';
-import albumRoutes from './routes/albums.routes.js';
+import songRoutes from './routes/song.routes.js';
+import albumRoutes from './routes/album.routes.js';
 import statsRoutes from './routes/stats.routes.js';
+import { connectDB } from './lib/db.js';
 
 dotenv.config();
 
@@ -15,10 +16,11 @@ const app = express();
 app.use('/api/users', userRoutes);
 app.use('/api/auth', authRoutes);
 app.use('/api/admin', adminRoutes);
-app.use('/api/songs', songsRoutes);
+app.use('/api/songs', songRoutes);
 app.use('/api/albums', albumRoutes);
 app.use('/api/stats', statsRoutes);
 
 app.listen(PORT, () => {
   console.log('Server is running on port', PORT);
+  connectDB();
 });
---
+++ backend/src/models/message.mode.js
@@ -0,0 +1,23 @@
+import mongoose from 'mongoose';
+
+const messageSchema = new mongoose.Schema(
+  {
+    senderId: {
+      type: String,
+      required: true,
+    },
+    receiverId: {
+      type: String,
+      required: true,
+    },
+    content: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export const Messages = mongoose.model('Messages', messageSchema);
