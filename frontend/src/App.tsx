6f5ecbc38c95c5f145ae7a241a76fd154f9f6392
cremacious <chrismackall3@gmail.com>
2025-06-09 16:11:54 -0400

    New layout

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,27 +2,26 @@ import { Routes, Route } from 'react-router';
 import HomePage from './pages/home/Homepage';
 import AuthCallbackPage from './pages/auth-callback/AuthCallbackPage';
 import { AuthenticateWithRedirectCallback } from '@clerk/clerk-react';
-// import { axiosInstance } from './lib/axios';
+import MainLayout from './layout/MainLayout';
+import ChatPage from './pages/chat/ChatPage';
 
 function App() {
-  // const fetchSomeData = async () => {
-  //   await axiosInstance.get('/users', {
-  //     headers: {
-  //       "Authorization": `Bearer ${token}`
-  //     }
-  //   });
-  // };
   return (
     <>
       <Routes>
-        <Route path="/" element={<HomePage />} />
         <Route
           path="/sso-callback"
           element={
-            <AuthenticateWithRedirectCallback signUpForceRedirectUrl={"/auth-callback"} />
+            <AuthenticateWithRedirectCallback
+              signUpForceRedirectUrl={'/auth-callback'}
+            />
           }
         />
         <Route path="/auth-callback" element={<AuthCallbackPage />} />
+        <Route element={<MainLayout />}>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/chat" element={<ChatPage />} />
+        </Route>
       </Routes>
     </>
   );
